d6f6532abf37e477fccf9b9f236ff83952d3e770
madden20202 <business.mocksock@gmail.com>
2022-08-15 12:26:12 -0500

    fill constructor and add final to properties

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,10 +1,10 @@
 class Transaction {
-  String id;
-  String title;
-  double amount;
+  final String id;
+  final String title;
+  final double amount;
   // this is non-primative data type, that comes shipped with flutter
-  DateTime date;
+  final DateTime date;
 
   // Constructor
-  Transaction();
+  Transaction({this.id, this.title, this.amount, this.date});
 }
