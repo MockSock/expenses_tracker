0cc9c6868d073fe22e34be8b1b8f89c4e557611d
madden20202 <business.mocksock@gmail.com>
2022-08-16 19:26:36 -0500

    add the transaction list code to the file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
+import 'package:expenses_tracker/models/transaction_list.dart';
 import 'package:flutter/material.dart';
-
-import 'package:intl/intl.dart';
-
-import './models/transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -98,71 +95,7 @@ class MyHomePage extends StatelessWidget {
           ),
 
           // Transaction List
-          Column(
-            // this allows a list of objects to become
-            // widgets via a function within map
-            children: transactions.map((tx) {
-              return Card(
-                // Always think "What shoud be inside my Widget"
-                // This helps plan the layout of the widgets and
-                // keeps you moving in a concrete direction
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.amber,
-                          width: 2,
-                        ),
-                      ),
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        // \ is an escape character
-                        "\$${tx.amount}",
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.amber,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            tx.title,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            // Easiest way to format and return as string
-                            DateFormat.yMMMd().format(tx.date),
-                            style: const TextStyle(
-                              fontSize: 14.0,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          TransactionList(),
         ],
       ),
     );
--- lib/models/transaction_list.dart
+++ lib/models/transaction_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import '../models/transaction.dart';
 
 class TransactionList extends StatefulWidget {
@@ -26,6 +28,70 @@ class TransactionListState extends State<TransactionList> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      // this allows a list of objects to become
+      // widgets via a function within map
+      children: _userTransactions.map((tx) {
+        return Card(
+          // Always think "What shoud be inside my Widget"
+          // This helps plan the layout of the widgets and
+          // keeps you moving in a concrete direction
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.amber,
+                    width: 2,
+                  ),
+                ),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  // \ is an escape character
+                  "\$${tx.amount}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      tx.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      // Easiest way to format and return as string
+                      DateFormat.yMMMd().format(tx.date),
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
