8633233ef3ac70320341a46439c4d9c1b3219f64
madden20202 <business.mocksock@gmail.com>
2022-08-18 11:41:16 -0500

    add framework for UserTransactions

---
+++ lib/widgets/user_transaction.dart
@@ -0,0 +1,14 @@
+// This file will manage the state of the other two transaction files
+import 'package:flutter/material.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
