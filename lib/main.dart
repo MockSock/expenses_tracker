0feef3a54fb5413c971af4e6a44ca046ad291cda
madden20202 <business.mocksock@gmail.com>
2022-08-18 11:24:37 -0500

    move new transaction work to new_transaction.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/transaction_list.dart';
+import './widgets/transaction_list.dart';
+import './widgets/new_transation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,16 +23,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  // Input Work
-  // Input values are always strings by default
-  // late String titleController;
-  // late String amountController;
-
-  // Controllers also are very useful,
-  // since they listen for input then save it
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,44 +47,8 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
 
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                // puts button on the right
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    // the controller will now track the value for you
-                    controller: titleController,
-                    // onChanged: (value) => titleController = value,
-                    decoration: const InputDecoration(labelText: 'Title'),
-                  ),
-                  TextField(
-                    controller: amountController,
-                    // onChanged: (value) => amountController = value,
-                    decoration: const InputDecoration(labelText: 'Amount'),
-                  ),
-
-                  // Submit Button
-                  TextButton(
-                    onPressed: () {
-                      print(titleController.text);
-                      print(amountController.text);
-                    },
-                    child: const Text(
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.green,
-                      ),
-                      'Add Transaction',
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          // New Transaction Area
+          NewTransaction(),
 
           // Transaction List
           TransactionList(),
--- lib/widgets/new_transation.dart
+++ lib/widgets/new_transation.dart
@@ -1,8 +1,55 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  // Input Work
+  // Input values are always strings by default
+  // late String titleController;
+  // late String amountController;
+
+  // Controllers also are very useful,
+  // since they listen for input then save it
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          // puts button on the right
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              // the controller will now track the value for you
+              controller: titleController,
+              // onChanged: (value) => titleController = value,
+              decoration: const InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: amountController,
+              // onChanged: (value) => amountController = value,
+              decoration: const InputDecoration(labelText: 'Amount'),
+            ),
+
+            // Submit Button
+            TextButton(
+              onPressed: () {
+                print(titleController.text);
+                print(amountController.text);
+              },
+              child: const Text(
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                ),
+                'Add Transaction',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
