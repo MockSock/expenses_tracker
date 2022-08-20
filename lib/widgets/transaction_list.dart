6e09c9d64c655b71567db20c3e4fb380bd2cc947
madden20202 <business.mocksock@gmail.com>
2022-08-20 15:46:29 -0500

    begin transaction to listview builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,73 +19,70 @@ class TransactionList extends StatelessWidget {
     // a container that gives it a fixed height, otherwise it is infinite
 
     // This is not very optimized, and bad on performance, so use the builder
-    return Container(
-      height: 350,
-      child: ListView(
-        // this allows a list of objects to become
-        // widgets via a function within map
-        children: transactions.map((tx) {
-          return Card(
-            // Always think "What shoud be inside my Widget"
-            // This helps plan the layout of the widgets and
-            // keeps you moving in a concrete direction
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.amber,
-                      width: 2,
-                    ),
-                  ),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    // \ is an escape character
-                    "\$${tx.amount}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                      color: Colors.amber,
-                    ),
+    return ListView.builder(
+      // this allows a list of objects to become
+      // widgets via a function within map
+      children: transactions.map((tx) {
+        return Card(
+          // Always think "What shoud be inside my Widget"
+          // This helps plan the layout of the widgets and
+          // keeps you moving in a concrete direction
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.amber,
+                    width: 2,
                   ),
                 ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        tx.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        // Easiest way to format and return as string
-                        DateFormat.yMMMd().format(tx.date),
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ),
-                  ],
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  // \ is an escape character
+                  "\$${tx.amount}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.amber,
+                  ),
                 ),
-              ],
-            ),
-          );
-        }).toList(),
-      ),
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      tx.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      // Easiest way to format and return as string
+                      DateFormat.yMMMd().format(tx.date),
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
