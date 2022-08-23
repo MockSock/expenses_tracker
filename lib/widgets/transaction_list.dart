6e19eea399145397fa135512fc563d095b2a6a8e
madden20202 <business.mocksock@gmail.com>
2022-08-23 17:55:58 -0500

    add placeholder for when there are no transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -17,72 +17,81 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     // This automatically gives scrolling functionality, but needs
     // a container that gives it a fixed height, otherwise it is infinite
-
-    // This is not very optimized, and bad on performance, so use the builder
-    return ListView.builder(
-      // shrinkwrap keeps the vertical space equal to teh amount of container space needed,
-      // without this, an infinite height error comes up
-      shrinkWrap: true,
-      itemCount: transactions.length,
-      itemBuilder: (context, index) {
-        // index will now track the specific trnasaction,
-        // letting flutter call for data based on the index
-        return Card(
-          // Always think "What shoud be inside my Widget"
-          // This helps plan the layout of the widgets and
-          // keeps you moving in a concrete direction
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
+    return Container(
+      height: 300,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  style: Theme.of(context).textTheme.bodyText1,
+                  'No Transactions',
                 ),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  ),
-                ),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                    // \ is an escape character
-                    // This makes it fixed to two decimal points, so it looks like currency
-                    "\$${transactions[index].amount.toStringAsFixed(2)}",
-                    style: Theme.of(context).textTheme.headline6),
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      transactions[index].title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
+                Image.asset('empty-list'),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                // index will now track the specific trnasaction,
+                // letting flutter call for data based on the index
+                return Card(
+                  // Always think "What shoud be inside my Widget"
+                  // This helps plan the layout of the widgets and
+                  // keeps you moving in a concrete direction
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                            // \ is an escape character
+                            // This makes it fixed to two decimal points, so it looks like currency
+                            "\$${transactions[index].amount.toStringAsFixed(2)}",
+                            style: Theme.of(context).textTheme.headline6),
                       ),
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      // Easiest way to format and return as string
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: const TextStyle(
-                        fontSize: 14.0,
-                        fontStyle: FontStyle.italic,
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              transactions[index].title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              // Easiest way to format and return as string
+                              DateFormat.yMMMd()
+                                  .format(transactions[index].date),
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
+                );
+              },
+            ),
     );
   }
 }
