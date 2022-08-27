703b22b3bfed90ec6ba7004c6436e83573b00865
madden20202 <business.mocksock@gmail.com>
2022-08-27 16:25:25 -0500

    prep to make spending bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 // This will stay stateless since it is just
 // displaying data that is passed down
 class FinanceChart extends StatelessWidget {
+  // Method to geenerate spending bars
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Text('I am the chart!'),
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
     );
   }
 }
