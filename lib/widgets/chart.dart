04b882531af0aa3644152d914c63dd8b28aa5116
madden20202 <business.mocksock@gmail.com>
2022-08-27 16:18:21 -0500

    notes on state management

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// This will stay stateless since it is just
+// displaying data that is passed down
 class FinanceChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
