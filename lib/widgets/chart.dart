346df8ab7ba373e86bbf7b5355d195b1c62ea19e
madden20202 <business.mocksock@gmail.com>
2022-08-27 16:44:36 -0500

    make a way to count back days

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -13,7 +13,10 @@ class FinanceChart extends StatelessWidget {
   List<Map<String, Object>> get groupedTransactionValues {
     // 7 is the length that index will take from
     return List.generate(7, (index) {
-      final weekday = DateTime.now();
+      final weekday = DateTime.now().subtract(
+        // will now display based on the seven days
+        Duration(days: index),
+      );
       // return a map
       return {'day': 'T', 'amount': '9.99'};
     });
