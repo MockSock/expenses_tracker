9696996327a0900f1fe4998f9fbf8ccbd60864e8
madden20202 <business.mocksock@gmail.com>
2022-09-07 17:06:18 -0500

    add basic values then an anitializer

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -3,6 +3,11 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+
+  ChartBar(this.label, this.value);
+
   @override
   Widget build(BuildContext context) {
     return Container();
