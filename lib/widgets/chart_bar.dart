974501e34acc9545ccdd5e14e2d0cbb700fac037
madden20202 <business.mocksock@gmail.com>
2022-09-07 17:19:01 -0500

    add final budgetpercent and constructor

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 class ChartBar extends StatelessWidget {
   final String label;
   final double spendingAmount;
+  final double totalBudgetPercent;
 
-  ChartBar(this.label, this.spendingAmount);
+  ChartBar(this.label, this.spendingAmount, this.totalBudgetPercent);
 
   @override
   Widget build(BuildContext context) {
