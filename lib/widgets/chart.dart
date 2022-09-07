d70f3c6c4ab14cea40e953aff7f346494170465a
madden20202 <business.mocksock@gmail.com>
2022-09-07 16:58:16 -0500

    simplify interpolation

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -47,7 +47,7 @@ class FinanceChart extends StatelessWidget {
         // now show values that were derived
         children: groupedTransactionValues.map((data) {
           return Text(
-            '${data['day']}' ' : ' '${data['amount']}',
+            '${data['day']}: ${data['amount']}',
           );
         }).toList(),
       ),
