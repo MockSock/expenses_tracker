cd88cbfd801664c224cc1b523c4167dbe4a9df38
madden20202 <business.mocksock@gmail.com>
2022-09-11 08:36:54 -0500

    change text widget to chart bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -52,9 +52,7 @@ class FinanceChart extends StatelessWidget {
         // now show values that were derived
         // overflow issue due to large amount of data
         children: groupedTransactionValues.map((data) {
-          return Text(
-            '${data['day']}: ${data['amount']}',
-          );
+          return ChartBar(label, spendingAmount, totalBudgetPercent);
         }).toList(),
       ),
     );
