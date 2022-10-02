273e5517734b6d4d27c9064f5f0188e9ccfb555c
madden20202 <business.mocksock@gmail.com>
2022-10-02 14:16:08 -0500

    add icon button in row

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -97,6 +97,8 @@ class TransactionList extends StatelessWidget {
                           ),
                         ],
                       ),
+                      // Delete button
+                      IconButton(),
                     ],
                   ),
                 );
