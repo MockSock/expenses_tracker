5b47206e7234ce7bb8dbad73cb268c0d31aaa8c0
madden20202 <business.mocksock@gmail.com>
2022-11-08 18:30:18 -0600

    remove unneeded container

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -18,101 +18,97 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This causes an issue in which the container with a fixed height is not accepting the
-    // listview.buileer, which will always be infinite, causing the app to crash
-    return Container(
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  style: Theme.of(context).textTheme.bodyText1,
-                  'No Transactions Added Yet',
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text(
+                style: Theme.of(context).textTheme.bodyText1,
+                'No Transactions Added Yet',
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'empty-list',
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'empty-list',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            )
-          // Still runs this builder for some reason
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (context, index) {
-                // index will now track the specific trnasaction,
-                // letting flutter call for data based on the index
-                return Card(
-                  // Always think "What shoud be inside my Widget"
-                  // This helps plan the layout of the widgets and
-                  // keeps you moving in a concrete direction
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                            // \ is an escape character
-                            // This makes it fixed to two decimal points, so it looks like currency
-                            "\$${transactions[index].amount.toStringAsFixed(2)}",
-                            style: Theme.of(context).textTheme.headline6),
+              ),
+            ],
+          )
+        // Still runs this builder for some reason
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (context, index) {
+              // index will now track the specific trnasaction,
+              // letting flutter call for data based on the index
+              return Card(
+                // Always think "What shoud be inside my Widget"
+                // This helps plan the layout of the widgets and
+                // keeps you moving in a concrete direction
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
                       ),
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              transactions[index].title,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                          // \ is an escape character
+                          // This makes it fixed to two decimal points, so it looks like currency
+                          "\$${transactions[index].amount.toStringAsFixed(2)}",
+                          style: Theme.of(context).textTheme.headline6),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            transactions[index].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
                             ),
                           ),
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              // Easiest way to format and return as string
-                              DateFormat.yMMMd()
-                                  .format(transactions[index].date)
-                                  .toString(),
-                              style: const TextStyle(
-                                fontSize: 14.0,
-                                fontStyle: FontStyle.italic,
-                              ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            // Easiest way to format and return as string
+                            DateFormat.yMMMd()
+                                .format(transactions[index].date)
+                                .toString(),
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              fontStyle: FontStyle.italic,
                             ),
                           ),
-                        ],
-                      ),
-                      // Delete button
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        // defaults to red but an be changed in theme
-                        color: Theme.of(context).errorColor,
-                        onPressed: () =>
-                            deleteTransaction(transactions[index].id),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-    );
+                        ),
+                      ],
+                    ),
+                    // Delete button
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      // defaults to red but an be changed in theme
+                      color: Theme.of(context).errorColor,
+                      onPressed: () =>
+                          deleteTransaction(transactions[index].id),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
   }
 }
