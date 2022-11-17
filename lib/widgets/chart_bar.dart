867dff9f8404272b694c226968e2a35db0ef34e8
madden20202 <business.mocksock@gmail.com>
2022-11-16 18:35:56 -0600

    move the chart bar to a layout builder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,52 +9,54 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        // Say spending total for day with no decimals
-        FittedBox(
-          // Makes it so there are no line breaks
-          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-        ),
-        // For spacing
-        const SizedBox(height: 4),
-        Container(
-          height: 60,
-          width: 10,
-          // allows for widgets to stack on top of
-          // each other creating a three dimensional space
-          child: Stack(
-            children: <Widget>[
-              // Bottom to top
-              Container(
-                // can also use Color.fromRGBO()
-                // to make custom colors
-                color: Colors.blueGrey,
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(15)),
-              ),
-              // This box will now be a portion of
-              // its parent widget in size
-              FractionallySizedBox(
-                // this is so as the budget gets used up more, the box fills
-                heightFactor: totalBudgetPercent,
-                child: Container(
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        children: <Widget>[
+          // Say spending total for day with no decimals
+          FittedBox(
+            // Makes it so there are no line breaks
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
+          // For spacing
+          const SizedBox(height: 4),
+          Container(
+            height: 60,
+            width: 10,
+            // allows for widgets to stack on top of
+            // each other creating a three dimensional space
+            child: Stack(
+              children: <Widget>[
+                // Bottom to top
+                Container(
+                  // can also use Color.fromRGBO()
+                  // to make custom colors
+                  color: Colors.blueGrey,
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+                // This box will now be a portion of
+                // its parent widget in size
+                FractionallySizedBox(
+                  // this is so as the budget gets used up more, the box fills
+                  heightFactor: totalBudgetPercent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 4),
-        Text(label),
-      ],
-    );
+          const SizedBox(height: 4),
+          Text(label),
+        ],
+      );
+    });
   }
 }
